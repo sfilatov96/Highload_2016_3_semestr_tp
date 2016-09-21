65a1ce52b3073fa46644570f0d61770a689d9984
sfilatov96 <stepa-filatov@yandex.ru>
2016-09-21 17:36:36 +0300

    final push

--- httpd.py
+++ httpd.py
@@ -1,40 +1,47 @@
 from files.help_utils import *
 from socket import *
-import threading
 import os
 import argparse
 
 BUFF = 1024
 HOST = '127.0.0.1'
 PORT = 80
-THREADS = 4
-thread_list = []
-def handler(serversock,number):
+WORKERS_COUNT = 2
+NCPU = 2
+forks = []
+
+
+def handler(serversock, pid, ROOT_DIR):
     while 1:
         print('waiting for connection... listening on port', PORT)
-        print("start thread %d" % number)
         conn, addr = serversock.accept()
         try:
-            parse(conn, addr)
-        except Exception :
+            parse(conn, addr, pid, ROOT_DIR)
+        except Exception:
             conn.close()
-
-
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='server')
     parser.add_argument('-p', type=int, help='port of server running')
-    parser.add_argument('-t', type=int, help='workers count')
+    parser.add_argument('-r', type=str, help='root document')
+    parser.add_argument('-n', type=str, help='number of CPU')
     args = vars(parser.parse_args())
     HOST = args['p'] or HOST
-    THREADS = args['t'] or THREADS
+    NCPU = args['n'] or NCPU
+    ROOT_DIR = args['r'] or ""
     ADDR = (HOST, PORT)
     serversock = socket(AF_INET, SOCK_STREAM)
     serversock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     serversock.bind(ADDR)
     serversock.listen(5)
-    for i in range(THREADS):
-        thread_list.append(threading.Thread(target=handler, args=(serversock, i+1)))
-    for t in thread_list:
-        t.start()
+    for x in range(0, WORKERS_COUNT * NCPU):
+        pid = os.fork()
+        forks.append(pid)
+        if pid == 0:
+            print('PID:', os.getpid())
+            handler(serversock, os.getpid(), ROOT_DIR)
+    serversock.close()
+
+    for pid in forks:
+        os.waitpid(pid, 0)
